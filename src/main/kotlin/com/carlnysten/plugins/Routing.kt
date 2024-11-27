225b5a4fc1a4073bd1025464f41a2c4ccf9d1b6d
Carl Nysten <nysten.carl@gmail.com>
2024-11-27 21:36:11 +0200

    Handle BadRequestException with Status Pages
    Respond with 400 Bad Request when BadRequestException is thrown.

--- src/main/kotlin/com/carlnysten/plugins/Routing.kt
+++ src/main/kotlin/com/carlnysten/plugins/Routing.kt
@@ -7,6 +7,7 @@ import com.carlnysten.routing.addTaskRoutes
 import com.carlnysten.routing.addUserRoutes
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -15,6 +16,9 @@ fun Application.configureRouting() {
     install(StatusPages) {
         exception<AuthenticationException> { call, _ ->
             call.respond(HttpStatusCode.Unauthorized)
+        }
+        exception<BadRequestException> { call, _ ->
+            call.respond(HttpStatusCode.BadRequest)
         }
         exception<Throwable> { call, cause ->
             call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
