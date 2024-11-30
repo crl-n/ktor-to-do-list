b6133a2fb81a6d081b98e6f0ddd0e28549addbc3
Carl Nysten <nysten.carl@gmail.com>
2024-11-30 13:50:02 +0200

    Add route test for GET /health/ready

--- src/test/kotlin/com/carlnysten/routing/HealthRoutesTest.kt
+++ src/test/kotlin/com/carlnysten/routing/HealthRoutesTest.kt
@@ -1,13 +1,20 @@
 package com.carlnysten.routing
 
+import com.carlnysten.config.DatabaseConfig
+import com.carlnysten.plugins.*
+import com.carlnysten.testcontainers.getPostgresContainer
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.testing.*
+import org.testcontainers.junit.jupiter.Container
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class HealthRoutesTest {
+
+    @Container
+    private val postgresContainer = getPostgresContainer()
 
     @Test
     fun `live endpoint returns 200 OK`() = testApplication {
@@ -18,6 +25,28 @@ class HealthRoutesTest {
         }
 
         val response = client.get("/health/live")
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
+
+    @Test
+    fun `ready endpoint returns 200 OK`() = testApplication {
+        application {
+            val dbConfig = DatabaseConfig(
+                jdbcUrl = postgresContainer.jdbcUrl,
+                user = postgresContainer.username,
+                password = postgresContainer.password
+            )
+            configureKoin()
+            configureSecurity()
+            configureSerialization()
+            routing {
+                addHealthRoutes()
+            }
+            configureDatabase(dbConfig)
+            runFlyway(dbConfig)
+        }
+
+        val response = client.get("/health/ready")
         assertEquals(HttpStatusCode.OK, response.status)
     }
 }
