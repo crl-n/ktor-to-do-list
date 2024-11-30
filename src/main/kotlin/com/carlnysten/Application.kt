e1a6382fd5c38d50c65d45cefe7122a5b0d363bc
Carl Nysten <nysten.carl@gmail.com>
2024-11-30 22:27:53 +0200

    Get database config values from application.conf

--- src/main/kotlin/com/carlnysten/Application.kt
+++ src/main/kotlin/com/carlnysten/Application.kt
@@ -9,10 +9,16 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    val dbName = environment.config.property("database.name").getString()
+    val dbPort = environment.config.property("database.port").getString()
+    val dbUser = environment.config.property("database.user").getString()
+    val dbPassword = environment.config.property("database.password").getString()
+
+
     val dbConfig = DatabaseConfig(
-        jdbcUrl = "jdbc:postgresql://localhost:5432/todolist",
-        user = "postgres",
-        password = "postgres"
+        jdbcUrl = "jdbc:postgresql://localhost:${dbPort}/${dbName}",
+        user = dbUser,
+        password = dbPassword,
     )
 
     configureKoin()
