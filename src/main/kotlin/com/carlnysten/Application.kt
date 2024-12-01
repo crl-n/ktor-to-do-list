417d049933e7c6f5bb04a3aa460ca7ac84bdf7a3
Carl Nysten <nysten.carl@gmail.com>
2024-12-02 01:04:47 +0200

    Optimize imports in Application.kt

--- src/main/kotlin/com/carlnysten/Application.kt
+++ src/main/kotlin/com/carlnysten/Application.kt
@@ -1,10 +1,8 @@
 package com.carlnysten
 
 import com.carlnysten.config.DatabaseConfig
-import com.carlnysten.models.dto.UserSession
 import com.carlnysten.plugins.*
 import io.ktor.server.application.*
-import io.ktor.server.sessions.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -15,7 +13,6 @@ fun Application.module() {
     val dbPort = environment.config.property("database.port").getString()
     val dbUser = environment.config.property("database.user").getString()
     val dbPassword = environment.config.property("database.password").getString()
-
 
     val dbConfig = DatabaseConfig(
         jdbcUrl = "jdbc:postgresql://localhost:${dbPort}/${dbName}",
